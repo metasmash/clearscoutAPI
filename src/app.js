0e02e0cbac55a8b3047e17c6c65b3b45d550aacb
FulconisYan <yan.fulconis@gmail.com>
2020-10-16 17:31:28 +0200

    tuto vendredi post

--- src/app.js
+++ src/app.js
@@ -5,23 +5,23 @@ const cors = require('cors')
 const app = express()
 const port = 8001
 const corsOptions = {
-  origin: 'http://146.59.144.55:3000',
+    origin: 'http://146.59.144.55:3000',
 }
 
 app.use(cors(corsOptions))
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
 })
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-  })
-
+})
 
 module.exports = app
