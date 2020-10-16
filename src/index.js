d2f54d5c43d9ead8a23b768390c441ed3f9fbe49
FulconisYan <yan.fulconis@gmail.com>
2020-10-16 15:24:06 +0200

    Mdr

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const TEST = require('../models/test')
 
 
 app.get(ROUTE.MAIN, (req,res,err) => {
+    console.log("Service Main Appelé")
+    console.log("Service Main Modifié")
     res.json(TEST)
 })
 
